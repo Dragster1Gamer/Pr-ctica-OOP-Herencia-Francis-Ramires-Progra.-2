8aa60cb9eb5b623191290f38380f8be47b510dc7
DragsWS <wandercastillo730@gmail.com>
2024-09-10 17:34:41 -0400

    Se agrego las tareas

---
+++ Tarea1P1/Program.cs
@@ -0,0 +1,152 @@
+﻿/*
+Nos dan una escuela. La escuela tiene clases de estudiantes. Cada clase tiene un conjunto de profesores. 
+Cada profesor imparte un conjunto de cursos. Los estudiantes tienen un nombre y un número único en la clase. 
+Las clases tienen un identificador de texto único. Los profesores tienen nombres. Los cursos tienen nombre, recuento de clases y recuento de ejercicios. 
+Tanto los profesores como los estudiantes son personas.
+
+Su tarea es desarrollar las clases (en términos de programación orientada a objetos) junto con sus atributos y operaciones/
+*/
+
+public abstract class Persona
+{
+    public string Nombre { get; set; }
+
+    public Persona(string nombre)
+    { Nombre = nombre; }
+
+}
+
+public class Estudiante : Persona
+{
+    public int Numero_Unico { get; set; }
+
+    public Estudiante(string nombre, int numero_unico) : base(nombre)
+    {
+        Numero_Unico = numero_unico;
+    }
+}
+
+
+
+public class Curso
+{
+    public string Nombre { get; set; }
+    public int NumeroDeClases { get; set; }
+    public int NumeroDeEjercicios { get; set; }
+
+    public Curso(string nombre, int numeroDeClases, int numeroDeEjercicios)
+    {
+        Nombre = nombre;
+        NumeroDeClases = numeroDeClases;
+        NumeroDeEjercicios = numeroDeEjercicios;
+    }
+}
+
+
+public class Profesor : Persona
+{
+    public List<Curso> Cursos { get; set; }
+
+    public Profesor(string nombre) : base(nombre)
+    {
+        Cursos = new List<Curso>();
+    }
+
+    public void AñadirCurso(Curso curso)
+    {
+        Cursos.Add(curso);
+    }
+}
+
+
+
+public class Clase
+{
+    public string Identificador { get; set; }
+    public List<Profesor> Profesores { get; set; }
+    public List<Estudiante> Estudiantes { get; set; }
+
+    public Clase(string identificador)
+    {
+        Identificador = identificador;
+        Profesores = new List<Profesor>();
+        Estudiantes = new List<Estudiante>();
+    }
+
+    public void AñadirProfesor(Profesor profesor)
+    {
+        Profesores.Add(profesor);
+    }
+
+    public void AñadirEstudiante(Estudiante estudiante)
+    {
+        Estudiantes.Add(estudiante);
+    }
+}
+
+
+public class Escuela
+{
+    public List<Clase> Clases { get; set; }
+
+    public Escuela()
+    {
+        Clases = new List<Clase>();
+    }
+
+    public void AñadirClase(Clase clase)
+    {
+        Clases.Add(clase);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Crear escuela
+        Escuela escuela = new Escuela();
+
+        // Crear clase
+        Clase clase1 = new Clase("Clase C");
+
+        // Crear estudiantes
+        Estudiante estudiante1 = new Estudiante("Wander Smerlin", 1);
+        Estudiante estudiante2 = new Estudiante("Smerlin Castillo", 2);
+
+        // Añadir estudiantes a la clase
+        clase1.AñadirEstudiante(estudiante1);
+        clase1.AñadirEstudiante(estudiante2);
+
+        // Crear profesor
+        Profesor profesor1 = new Profesor("Francis Ramirez");
+
+        // Crear cursos
+        Curso cursoMatematicas = new Curso("Matemáticas", 10, 50);
+        Curso cursoProgramacion = new Curso("Física", 8, 40);
+
+        // Asignar cursos al profesor
+        profesor1.AñadirCurso(cursoMatematicas);
+        profesor1.AñadirCurso(cursoProgramacion);
+
+        // Añadir profesor a la clase
+        clase1.AñadirProfesor(profesor1);
+
+        // Añadir la clase a la escuela
+        escuela.AñadirClase(clase1);
+
+        // Mostrar detalles
+        Console.WriteLine($"Escuela tiene {escuela.Clases.Count} clase(s)");
+        Console.WriteLine($"Clase {clase1.Identificador} tiene {clase1.Estudiantes.Count} estudiante(s) y {clase1.Profesores.Count} profesor(es)");
+    }
+}
---
+++ Tarea1P2/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>
+        {
+            new Rectangle(25, 5),
+            new Triangle(15, 5),
+            new Circle(9)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"El área del {shape.GetType().Name} es: {shape.CalculateSurface()}");
+        }
+    }
+}
+
+
+
+
+
+public abstract class Shape
+{
+    public double Alto { get; set; }
+    public double Ancho { get; set; }
+
+    public Shape(double alto, double ancho)
+    {
+        Alto = alto;
+        Ancho = ancho;
+    }
+
+    public abstract double CalculateSurface();
+
+
+}
+
+class Rectangle : Shape
+{
+    public Rectangle(double alto, double ancho) : base(alto, ancho) { }
+
+    public override double CalculateSurface()
+    {
+        return Alto * Ancho;
+    }
+}
+
+
+class Triangle : Shape
+{
+    public Triangle(double alto, double ancho) : base(alto, ancho) { }
+
+    public override double CalculateSurface()
+    {
+        return Alto * Ancho /2;
+    }
+}
+
+class Circle : Shape
+{
+    public Circle(double radius) : base(radius, radius) { }
+
+    public override double CalculateSurface()
+    {
+        return Math.PI * (Alto * Alto);
+    }
+}
